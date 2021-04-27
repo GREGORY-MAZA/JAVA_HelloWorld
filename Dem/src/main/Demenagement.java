396f969bd76b4d48038f840a33fa179332aecb07
Greg <maza.gregory46@gmail.com>
2021-04-27 15:49:00 +0200

    5 - Déménagement

---
+++ Dem/src/main/Demenagement.java
@@ -0,0 +1,30 @@
+package main;
+
+public class Demenagement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int cartonsAncienLocal = 34;
+		int cartonsNouveauLocal = 0;
+		int cartonsCamion = 0;
+		
+		while(cartonsAncienLocal != 0) {
+			if (cartonsCamion < 9) {
+				cartonsAncienLocal -= 1;
+				cartonsCamion += 1;
+			} else {
+			System.out.println("Le camion vient de prendre " + cartonsCamion + " cartons de l'ancien local");
+			cartonsNouveauLocal += cartonsCamion;
+			cartonsCamion = 0;
+			System.out.println("Le nouveau local contient " + cartonsNouveauLocal + " cartons");
+			}
+			
+		}if (cartonsAncienLocal == 0) {
+			cartonsNouveauLocal += cartonsCamion;
+			System.out.println("Livraison de " +cartonsCamion+ " cartons restant");
+			System.out.println("Le nouveau local est en bordel avec les " +cartonsNouveauLocal+ " cartons");
+		}
+
+	}
+
+}
